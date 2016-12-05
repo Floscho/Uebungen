6fe526c5fda3c9b275b38810fae27a1e2460a7bc
Teilnehmer <Teilnehmer@BBQTEIL-AJMR0RE.Bildung.zz>
2016-12-05 14:27:29 +0100

    Card erstellt
    Rolf´s Version mit Konstruktor in Card

---
+++ Users/Teilnehmer/Documents/NetBeansProjects/JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author Teilnehmer
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        int a, b, c = 0;
+        int a, b, c;
+//        int g, int h, int i = 0;
+        int d, e, F;
+//        Int k, l, m = 0;
+    }
+
+}
